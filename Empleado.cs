844b7a4a47ca3d800dc20cecbd02fa4f415a0f61
jacqueline <jacqueline@JACQUIE>
2021-06-28 19:29:29 -0500

    Add project files.

---
+++ Empleado.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Empleado
+{
+    class Empleado
+    {
+        /*Declara los datos miembros indicados y aplica el encapsulamiento utilizando los
+        modificadores de acceso necesarios*/
+
+        private double salarioMensual;
+        private double bonoMensual;
+        private double salarioAnual;
+        private int numeroEmpleado;
+        private String nombre;
+        private String numeroTelefonico;
+        private String correoElectronico;
+        private static int contadorEmpleado;
+
+        //constructor de un dato
+
+        public Empleado(String nombre)
+        {
+            this.nombre = nombre;
+        }
+
+        //constructor de dos datos
+        public Empleado(String nombre, double salarioMensual)
+        {
+            this.nombre = nombre;
+            this.salarioMensual = salarioMensual;
+
+        }
+
+        //constructor de tres datos
+        public Empleado(String nombre, double salarioMensual, double bonoMensual)
+        {
+            this.nombre = nombre;
+            this.salarioMensual = salarioMensual;
+            this.bonoMensual = bonoMensual;
+        }
+
+        //constructor que toma parametros y en base a esto asigna el salario y bono si es negativo convierte a 0
+        public Empleado(double salarioMensual, double bonoMensual, String nombre, String numeroTelefonico, String correoElectronico)
+        {
+            this.nombre = nombre;
+            this.numeroTelefonico = numeroTelefonico;
+            this.correoElectronico = correoElectronico;
+            //Incrementa el contador al crear un objeto nuevo
+            Empleado.contadorEmpleado++;
+            //Asigna el valor del contador al numeroEmpleado
+            this.numeroEmpleado = Empleado.contadorEmpleado;
+            //Si el salario mensual no es positivo, debe establecerse en 0.0
+            if (salarioMensual < 0)
+            {
+                salarioMensual = 0;
+            }
+            else
+            {
+                this.salarioMensual = salarioMensual;
+            }
+            //Si el bono mensual no es positivo, debe establecerse en 0.0
+            if (bonoMensual < 0)
+            {
+                bonoMensual = 0;
+            }
+            else
+            {
+                this.bonoMensual = bonoMensual;
+            }
+        }
+
+        //propiedad para modificar salarioMensual
+        public double SalarioMensual
+        {
+            get { return this.salarioMensual; }
+
+            set { this.salarioMensual = salarioMensual; }
+        }
+
+        //propiedad para modificar bonoMensual
+        public double BonoMensual
+        {
+            get { return this.bonoMensual; }
+
+            set { this.bonoMensual = bonoMensual; }
+        }
+
+        //propiedad para obtener el salarioAnual
+        public double SalarioAnual
+        {
+            get { return this.salarioAnual; }
+
+
+        }
+
+        //propiedad para obtener el numeroEmpleado
+        public int NumeroEmpleado
+        {
+            get { return this.numeroEmpleado; }
+        }
+
+        //propiedad para obtener el nombre
+        public String Nombre
+        {
+            get { return this.nombre; }
+            set { this.nombre = nombre; }
+        }
+
+        //propiedad para  obtener numeroTelefonico
+        public String NumeroTelefonico
+        {
+            get { return this.numeroTelefonico; }
+            set { this.numeroTelefonico = numeroTelefonico; }
+        }
+
+        //propiedad para obtener correoElectronico
+        public String CorreoElectronico
+        {
+            get { return this.correoElectronico; }
+            set { this.correoElectronico = correoElectronico; }
+        }
+
+        //Sobrecarga  de  metodos para asignarSalarioMensual
+        //Metodo para asignar el salario mensual
+        public void asignarSalarioMensual(string nombre, double salarioMensual)
+        {
+            this.nombre =nombre;
+            this.salarioMensual = salarioMensual;
+
+
+            //return this.salarioMensual;
+        }
+        //Metodo 2 para asignar el salario mensual
+        public void asignarSalarioMensual( double salarioMensual)
+        {
+            this.nombre = nombre;
+            this.salarioMensual = salarioMensual;
+   
+            //return this.salarioMensual;
+        }
+
+        //Metodo para asignar el salario mensual con el bono
+        public void asignarSalarioMensual(string nombre, double salarioMensual, double bonoMensual)
+        {
+            this.nombre = nombre;
+            this.salarioMensual = salarioMensual + bonoMensual;
+  
+            //return this.salarioMensual;
+        }
+
+        //Sobrecarga de metodos para asignarBonoMensual
+        //Metodo para asignar el bono mensual
+        public void asignarBonoMensual(double bonoMensual)
+        {
+            this.bonoMensual = bonoMensual;
+
+            //return bonoMensual;
+        }
+
+        //Metodo para asignar el bono mensual 2
+        public void asignarBonoMensual(string nombre, double salarioMensual, double bonoMensual)
+        {
+            this.nombre = nombre;
+            this.salarioMensual = salarioMensual;
+            this.bonoMensual = bonoMensual;
+
+
+
+            //return bonoMensual;
+        }
+
+        //Método de aumentar el 10% de su salario 
+        public double aumentarSalario(double salarioMensual)
+        {
+            this.salarioMensual = (salarioMensual * .10) + salarioMensual;
+            return this.salarioMensual;
+
+        }
+
+        //El método para Mostrar el salario anual del empleado
+        public double mostrarSalarioAnual(double salarioMensual, double bonoMensual, Boolean aumento)
+        {
+            if (aumento == true)
+            {
+                salarioMensual = aumentarSalario(salarioMensual);
+                this.salarioAnual = (salarioMensual * 12) + (bonoMensual * 12);
+                return this.salarioAnual;
+
+            }
+            else
+            {
+                this.salarioAnual = (salarioMensual * 12) + (bonoMensual * 12);
+                return this.salarioAnual;
+            }
+            return this.salarioAnual;
+        }
+
+
+        //override
+        public String toString()
+        {
+            return " \n Numero de Empleado: " + numeroEmpleado + "\n Nombre: "
+            + nombre + " \n Telefono: " + numeroTelefonico + "\n Correo electronico: " + correoElectronico
+            + "\n Bono: " + bonoMensual + "\n Salario Anual: " + salarioAnual;
+
+        }
+
+        //destructor
+        ~Empleado()
+        {
+
+            this.salarioMensual = 0;
+            this.bonoMensual = 0;
+            this.salarioAnual = 0;
+            this.numeroEmpleado = 0;
+            this.nombre = null;
+            this.numeroTelefonico = null;
+            this.correoElectronico = null;
+
+           // Console.WriteLine("El destructor ha sido llamado");
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Empleado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //double salarioMensual, double bonoMensual,String nombre, String numeroTelefonico, String correoElectronico
+
+
+            Empleado empleado1 = new Empleado(10000, -500, "Jacqueline", "123456789", "jacquelinem@un.com");
+            Console.WriteLine("\nDatos del empleado antes de aumentar el salario y antes de calcular el salario anual");
+            Console.WriteLine("Empleado1: " + empleado1.toString());
+            Console.WriteLine(" Salario Mensual: " + empleado1.SalarioMensual);
+            empleado1.aumentarSalario(10000);
+            empleado1.mostrarSalarioAnual(10000, 500, true);
+            Console.WriteLine("\nDatos del empleado despues de aumentar el 10% del salario y de calcular  el salario anual ");
+            Console.WriteLine("Empleado1: " + empleado1.toString());
+            Console.WriteLine(" Salario Mensual: " + empleado1.SalarioMensual);
+
+            Empleado empleado2 = new Empleado(8000, 300, "Juan", "103456799", "juan@un.com");
+            Console.WriteLine("\nDatos del empleado antes de aumentar el salario y antes de calcular el salario anual");
+            Console.WriteLine("Empleado2: " + empleado2.toString());
+            Console.WriteLine(" Salario Mensual: " + empleado2.SalarioMensual);
+            empleado2.aumentarSalario(8000);
+            empleado2.mostrarSalarioAnual(8000, 300, true);
+            Console.WriteLine("\nDatos del empleado despues de aumentar el 10% del salario y de calcular  el salario anual ");
+            Console.WriteLine("Empleado2: " + empleado2.toString());
+            Console.WriteLine(" Salario Mensual: " + empleado2.SalarioMensual);
+
+            Console.WriteLine("\nEn este ejemplo (empleado3) se crea un objeto utilizando el constructor con 1 dato: nombre");
+
+            Empleado empleado3 = new Empleado("Jacquie");
+            Console.WriteLine("\nEmpleado3: " + empleado3.Nombre);
+
+            Console.WriteLine("\nEn este ejemplo (empleado4) se crea un objeto utilizando el constructor con 2 datos: nombre y salario");
+            Console.WriteLine("\nSe aplica la sobrecarga de metodos del metodo asignarSalarioMensual ");
+
+            Empleado empleado4 = new Empleado("Darla", 11000);
+            Console.WriteLine("\nEmpleado4: " + "\nNombre: " + empleado4.Nombre + "\nSalario Mensual: " + empleado4.SalarioMensual);
+            empleado4.asignarSalarioMensual("Sara", 9000);
+            Console.WriteLine("\nEmpleado4:  " + " Cambio de nombre y de salario: " );
+            Console.WriteLine("\nEmpleado4: " + "\nNombre: " + empleado4.Nombre + "\nSalario Mensual: " + empleado4.SalarioMensual);
+            Console.WriteLine("\nEmpleado4: " + "Cambio/ asignar salario" );
+            empleado4.asignarSalarioMensual(9500);
+            Console.WriteLine("\nEmpleado4: " + "\nNombre: " + empleado4.Nombre + "\nSalario Mensual: " + empleado4.SalarioMensual);
+
+            Console.WriteLine("\nEn este ejemplo (empleado5) se crea un objeto utilizando el constructor con 3 datos: nombre, " +
+                "salario mensual y bono mensual");
+            Console.WriteLine("\nSe aplica la sobrecarga de metodos del metodo asignarSalarioMensual ");
+
+            Empleado empleado5 = new Empleado("Jose", 11500, 325);
+            Console.WriteLine("\nEmpleado5: " + "\n Nombre: " + empleado5.Nombre + "\nSalario Mensual: " + empleado5.SalarioMensual
+                + "\nBono Mensual: " + empleado5.BonoMensual);
+            Console.WriteLine("\nEmpleado5: " + "Cambio de salario");
+            empleado5.asignarSalarioMensual(11000);
+            Console.WriteLine("\nEmpleado5: " + "\n Nombre: " + empleado5.Nombre + "\nSalario Mensual: " + empleado5.SalarioMensual
+               + "\nBono Mensual: " + empleado5.BonoMensual);
+            empleado5.asignarSalarioMensual("Carlos", 12000);
+            Console.WriteLine("\nEmpleado5: " + " Cambio de nombre y de salario: " );
+            Console.WriteLine("\nEmpleado5: " + "\n Nombre: " + empleado5.Nombre + "\nSalario Mensual: " + empleado5.SalarioMensual
+               + "\nBono Mensual: " + empleado5.BonoMensual);
+            Console.WriteLine("\nEmpleado5: " + " Cambio de nombre, de salario y asigna bono mensual : ");
+            empleado5.asignarSalarioMensual("Joaquin", 12500, 500);
+            Console.WriteLine("\nEmpleado5: " + "\n Nombre: " + empleado5.Nombre + "\nSalario Mensual: " + empleado5.SalarioMensual
+               + "\nBono Mensual: " + empleado5.BonoMensual);
+            Console.WriteLine("\nEmpleado5: " + "Cambio de bono ");
+            empleado5.asignarBonoMensual(700);
+            Console.WriteLine("\nEmpleado5: " + "\n Nombre: " + empleado5.Nombre + "\nSalario Mensual: " + empleado5.SalarioMensual
+               + "\nBono Mensual: " + empleado5.BonoMensual);
+            Console.WriteLine("\nEmpleado5: " + "Se utiliza el constructor para asignar nombre, salario mensual y bono");
+            empleado5.asignarBonoMensual("Joaquin", 15000, 100);
+            Console.WriteLine("\nEmpleado5: " + "\n Nombre: " + empleado5.Nombre + "\nSalario Mensual: " + empleado5.SalarioMensual
+               + "\nBono Mensual: " + empleado5.BonoMensual);
+        }
+    }
+}
